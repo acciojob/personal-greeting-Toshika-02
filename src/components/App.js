231a711ccedf0730e5f3865d35a160f030275abd
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-04-11 21:31:58 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,16 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-   const [name, setName] = React.useState(""); // Using React.useState for state management
 
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  }
+  const [name, setName] = useState("");
   return (
     <div>
-        {/* Do not remove the main div */}
-     <input
-        type="text"
-        placeholder="Enter your name"
-        value={name}
-        onChange={handleNameChange}
-      />
-      {name && <p>Hello, {name}! Nice to meet you.</p>}
+        <p>Enter your name:</p>
+        <input onChange={(e) => setName(e.target.value)}></input>
+        <p>{name}</p>
     </div>
-  );
+  )
 }
+
 export default App
